02e8139b8d5b04787d5737a244bae48dedfd7834
audrie <audrie.talbot@gmail.com>
2015-05-08 14:28:16 -0700

    Pass the save spec for class stylist.

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -5,4 +5,12 @@ describe(Stylist) do
     it('starts off with no stylists') do
     end
   end
+
+  describe("#save") do
+    it('lets you save stylists to the database') do
+      test_stylist = Stylist.new({:name => 'Barbara', :id => nil})
+      test_stylist.save()
+      expect(test_stylist.id()).to(be_an_instance_of(Fixnum))
+    end
+  end
 end
